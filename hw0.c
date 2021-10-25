e8dc5a9d3f780ec17a48a8bcf4e378974f79547e
e1iav <eliav.opa@gmail.com>
2021-10-25 19:57:13 +0300

    added user input to bases a,b. doesn't yet check conditions.

---
+++ hw0.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	int a, b;
+	puts("> Please enter the numbers base:");
+	scanf("%d", &a);
+
+	puts("> Please enter the desired base:");
+	scanf("%d", &b);
+
+	return 0;
+}

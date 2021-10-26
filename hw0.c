8f65f4a841ca6378fac620d4b9c86f90ef753b8e
e1iav <eliav.opa@gmail.com>
2021-10-26 15:28:11 +0300

    added assertions in the bases input

--- hw0.c
+++ hw0.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+#include <assert.h>
 
 int main() {
 	int a, b;
 	puts("> Please enter the numbers base:");
 	scanf("%d", &a);
+	assert(2<=a && a<=16 && "> Invalid numbers base");
 
 	puts("> Please enter the desired base:");
 	scanf("%d", &b);
+	assert(2<=b && b<=16 && "> Invalid desired base");
 
 	return 0;
 }

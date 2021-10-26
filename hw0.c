d211e28e87579bec3c6528da240cc2c799330717
e1iav <eliav.opa@gmail.com>
2021-10-26 18:40:39 +0300

    now converts number in base a to natural base and prints

--- hw0.c
+++ hw0.c
@@ -3,6 +3,8 @@
 
 int main() {
 	int a, b;
+	int num = 0;
+	char c;
 	puts("> Please enter the numbers base:");
 	scanf("%d", &a);
 	assert(2<=a && a<=16 && "> Invalid numbers base");
@@ -10,6 +12,22 @@ int main() {
 	puts("> Please enter the desired base:");
 	scanf("%d", &b);
 	assert(2<=b && b<=16 && "> Invalid desired base");
+
+	printf("> Please enter a number in base %d:", a);
+	getchar();
+	for(;;) {
+		c = (char)getchar();
+		if (c == EOF || c=='\n') break;
+		if (a<=10)
+			assert(c>='0' && c<='9'-10+a && "> Invalid number!");
+		else
+			assert(c>='0' && c<='9' || c>='A' && c<='F'-16+a && "> Invalid number!");
+		
+		num*=a;
+		if (c<='9') num += c-'0';
+		else num += c-'A';
+	}
+	printf("%d\n", num);
 
 	return 0;
 }
